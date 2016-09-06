3f1fae1012101426764e641a1f4eeeae9e8680d2
Thomas Burns <thomasburns@Thomass-MacBook-Pro.local>
2016-09-06 14:43:01 -0700

    1st Attempt User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
+
   get 'welcome/index'
 
   get 'welcome/about'
 
-  root 'welcome#index'
+  root 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
